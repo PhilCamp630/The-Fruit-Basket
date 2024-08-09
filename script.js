a7456756fc09ca394da503213b8398ad42421546
Madi Bromfield <madibromfield215@gmail.com>
2024-08-09 17:47:00 -0500

    woohoo

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
 // Define fruits with their attributes
 const fruits = [
-    // Red fruits
-    { name: "Apple", color: "red", isCitrus: false },
-    { name: "Strawberry", color: "red", isCitrus: false },
-    { name: "Raspberry", color: "red", isCitrus: false },
-    { name: "Cherry", color: "red", isCitrus: false },
-    { name: "Red Grapes", color: "red", isCitrus: false },
+  // Red fruits
+  { name: "Apple", color: "red", isCitrus: false },
+  { name: "Strawberry", color: "red", isCitrus: false },
+  { name: "Raspberry", color: "red", isCitrus: false },
+  { name: "Cherry", color: "red", isCitrus: false },
+  { name: "Red Grapes", color: "red", isCitrus: false },
 
-    // Yellow fruits
-    { name: "Banana", color: "yellow", isCitrus: false },
-    { name: "Mango", color: "yellow", isCitrus: false },
-    { name: "Pineapple", color: "yellow", isCitrus: true },
-    { name: "Cantaloupe", color: "yellow", isCitrus: false },
-    { name: "Lemon", color: "yellow", isCitrus: true },
+  // Yellow fruits
+  { name: "Banana", color: "yellow", isCitrus: false },
+  { name: "Mango", color: "yellow", isCitrus: false },
+  { name: "Pineapple", color: "yellow", isCitrus: true },
+  { name: "Cantaloupe", color: "yellow", isCitrus: false },
+  { name: "Lemon", color: "yellow", isCitrus: true },
 
-    // Green fruits
-    { name: "Lime", color: "green", isCitrus: true },
-    { name: "Kiwi", color: "green", isCitrus: false },
-    { name: "Avocado", color: "green", isCitrus: false },
-    { name: "Green Apple", color: "green", isCitrus: false },
-    { name: "Green Grapes", color: "green", isCitrus: false },
+  // Green fruits
+  { name: "Lime", color: "green", isCitrus: true },
+  { name: "Kiwi", color: "green", isCitrus: false },
+  { name: "Avocado", color: "green", isCitrus: false },
+  { name: "Green Apple", color: "green", isCitrus: false },
+  { name: "Green Grapes", color: "green", isCitrus: false },
 
-    // Blue fruits
-    { name: "Blueberry", color: "blue", isCitrus: false },
-    { name: "Blackberry", color: "blue", isCitrus: false },
-    { name: "Blue Grape", color: "blue", isCitrus: false },
-    { name: "Blue Plum", color: "blue", isCitrus: false },
-    { name: "Elderberry", color: "blue", isCitrus: false },
+  // Blue fruits
+  { name: "Blueberry", color: "blue", isCitrus: false },
+  { name: "Blackberry", color: "blue", isCitrus: false },
+  { name: "Blue Grape", color: "blue", isCitrus: false },
+  { name: "Blue Plum", color: "blue", isCitrus: false },
+  { name: "Elderberry", color: "blue", isCitrus: false },
 
-    // Orange fruits
-    { name: "Orange", color: "orange", isCitrus: true },
-    { name: "Peach", color: "orange", isCitrus: false },
-    { name: "Tangerine", color: "orange", isCitrus: true },
-    { name: "Papaya", color: "orange", isCitrus: false },
-    { name: "Apricot", color: "orange", isCitrus: false },
+  // Orange fruits
+  { name: "Orange", color: "orange", isCitrus: true },
+  { name: "Peach", color: "orange", isCitrus: false },
+  { name: "Tangerine", color: "orange", isCitrus: true },
+  { name: "Papaya", color: "orange", isCitrus: false },
+  { name: "Apricot", color: "orange", isCitrus: false },
 
-    // Purple fruits
-    { name: "Grapes", color: "purple", isCitrus: false },
-    { name: "Plum", color: "purple", isCitrus: false },
-    { name: "Elderberry", color: "purple", isCitrus: false },
-    { name: "Blackberry", color: "purple", isCitrus: false },
-    { name: "Purple Grape", color: "purple", isCitrus: false }
+  // Purple fruits
+  { name: "Grapes", color: "purple", isCitrus: false },
+  { name: "Plum", color: "purple", isCitrus: false },
+  { name: "Elderberry", color: "purple", isCitrus: false },
+  { name: "Blackberry", color: "purple", isCitrus: false },
+  { name: "Purple Grape", color: "purple", isCitrus: false },
 ];
 
 const fruitBank = [];
@@ -54,3 +54,4 @@ const citrusFruits = [];
 const nonCitrusFruits = [];
 
 // Your JavaScript logic goes here
+const addFruit = (event) => {};
